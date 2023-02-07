83b1dd9d3a45cc3a16fa3e7b3e19b30c316211f2
Giuseppe <g.dipuglia@datawizard.it>
2023-02-07 12:47:34 +0100

    fixed productlist

--- apps/productlist/assets/js/productlist.js
+++ apps/productlist/assets/js/productlist.js
@@ -26,14 +26,36 @@ fetch(url)
                 var t = $('#prod-table').DataTable();
                 t.pageLength=2;
                 t.iDisplayLength= 50;
-                t.row.add( [
-                    posts.entry[prod].resource.id,
-                    '<b>'+posts.entry[prod].resource.name[0].productName,
-                    posts.entry[prod].resource.name[0].countryLanguage[0].country.coding[0].display,
-                    '<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">Viewer</a> <a target="_blank" href="https://jpa.unicom.datawizard.it/apps/visualiser/index.html?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">New Viewer</a>',
-                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=xml">XML</a> &nbsp;&nbsp;&nbsp; <a href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=json">JSON</a>',
-                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'/$validate">FHIR Validation</a>'
-                ] ).draw();
+                current_row = []
+                try { current_row.push(posts.entry[prod].resource.id)
+                } catch (error){ current_row.push(error) }
+
+                try { current_row.push('<b>'+posts.entry[prod].resource.name[0].productName+'</b>')
+                } catch (error){ current_row.push(error) }
+
+                try { current_row.push(posts.entry[prod].resource.name[0].usage[0].country.coding[0].display)
+                } catch (error){ current_row.push(error) }
+
+                try { current_row.push( '<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">Viewer</a> <a target="_blank" href="https://jpa.unicom.datawizard.it/apps/visualiser/index.html?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">New Viewer</a>' )
+                } catch (error){ current_row.push(error) }
+
+                try { current_row.push( '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=xml">XML</a> &nbsp;&nbsp;&nbsp; <a href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=json">JSON</a>' )
+                } catch (error){ current_row.push(error) }
+
+                try { current_row.push( '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'/$validate">FHIR Validation</a>' )
+                } catch (error){ current_row.push(error) }
+
+                t.row.add(
+                    current_row
+//                [
+//                    posts.entry[prod].resource.id,
+//                    '<b>'+posts.entry[prod].resource.name[0].productName,
+//                    posts.entry[prod].resource.name[0].usage[0].country.coding[0].display,
+//                    '<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">Viewer</a> <a target="_blank" href="https://jpa.unicom.datawizard.it/apps/visualiser/index.html?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">New Viewer</a>',
+//                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=xml">XML</a> &nbsp;&nbsp;&nbsp; <a href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=json">JSON</a>',
+//                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'/$validate">FHIR Validation</a>'
+//                ]
+                ).draw();
             }
         }
     )

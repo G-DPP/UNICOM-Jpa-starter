43cbec019d26529c5231771df8c91b376a87c7bc
Simone Diaco <simodiaco@gmail.com>
2023-02-27 18:28:00 +0100

    edited import_data

--- python_scripts/import_data.py
+++ python_scripts/import_data.py
@@ -6,6 +6,7 @@ import urllib.request
 import pathlib
 from dotenv import load_dotenv
 import shutil
+import time
 
 load_dotenv()
 
@@ -25,45 +26,70 @@ if os.path.isdir(result_dir):
     shutil.rmtree(result_dir)
 os.mkdir("./output")
 
+resource_list = [
+    'codesystem',
+    'valueset',
+    'structuredefinition', #TODO: 400
+    # 'bundle',
+    'medicinalproductdefinition',
+    'organization',
+    'manufactureditemdefinition',
+    'packagedproductdefinition',
+    'regulatedauthorization',
+    'administrableproductdefinition',
+    'ingredient',
+]
+
+
+def create_or_update(request_url, resource_type, resource_id, data):
+    headers = {
+        "Content-Type": "application/fhir+json"
+    }
+    if resource_type.lower() == "structuredefinition" or resource_type.lower() == 'bundle':
+        response = requests.post(request_url, json=data)
+    else:
+        url = f"{request_url}/{resource_id}"
+        data = json.dumps(data)
+        response = requests.put(url, headers=headers, data=data.encode())
+    return response
+
 
 def main():
-    urllib.request.urlretrieve(package_url, "package.tgz")
-    file = tarfile.open('package.tgz')
-    file.extractall('./')
+    # urllib.request.urlretrieve(package_url, "package.tgz")
+    # file = tarfile.open('package.tgz')
+    # file.extractall('./')
     results = {}
 
-    for file_path in pathlib.Path("./package").glob('**/*.json'):
-        with open(file_path, "r") as json_file:
-            file_name = str(file_path).rsplit('/', 1)[1]
-            json_dict = json.load(json_file)
-            resource_type = json_dict.get('resourceType', '')
+    for item in resource_list:
+        for file_path in pathlib.Path("./package").glob('**/*.json'):
+            with open(file_path, "r") as json_file:
+                file_name = str(file_path).rsplit('/', 1)[1]
+                json_dict = json.load(json_file)
+                resource_type = json_dict.get('resourceType', '')
+                resource_id = json_dict.get('id', '')
+                type = json_dict.get("type", '')
 
-            type = json_dict.get("type", '')
+                if resource_type.lower() != item:
+                    continue
+                print("#-FILENAME "+file_name)
 
-            # if resource_type.lower() != 'bundle':
-            #     continue
-            print("#-FILENAME "+file_name)
+                request_url = f"{server_url}{resource_type}" if isinstance(type, dict) or type.lower() not in ["transaction", "batch"] else server_url
+                a = create_or_update(request_url, resource_type, resource_id, json_dict)
 
-            # TODO: gestire un ordine?
-            #  CodeSystem - ValueSet - StructureDefinition
-            #  Bundle
+                request_result_dir = f"{result_dir}/{a.status_code}"
+                request_result_filepath = f"{request_result_dir}/{file_name}"
+                if not os.path.isdir(request_result_dir):
+                    os.mkdir(request_result_dir)
 
-            # if not 'StructureDefinition' in file_name:
-            #     continue
-            request_url = f"{server_url}{resource_type}" if isinstance(type, dict) or type.lower() not in ["transaction", "batch"] else server_url
-            a = requests.post(request_url, json=json_dict)
+                with open(request_result_filepath, "w") as outfile:
+                    json.dump(a.json(), outfile, indent=2)
 
-            request_result_dir = f"{result_dir}/{a.status_code}"
-            request_result_filepath = f"{request_result_dir}/{file_name}"
-            if not os.path.isdir(request_result_dir):
-                os.mkdir(request_result_dir)
+                results.setdefault(a.status_code, []).append(file_path)
 
-            with open(request_result_filepath, "w") as outfile:
-                json.dump(a.json(), outfile, indent=2)
+                print(f"{file_path} -- {a.status_code}")
+        print(f"FINISH RESOURCE {item}")
+        time.sleep(3)
 
-            results.setdefault(a.status_code, []).append(file_path)
-
-            print(f"{file_path} -- {a.status_code}")
     print("DONE!")
 
 

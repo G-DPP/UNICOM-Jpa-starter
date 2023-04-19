c0f5ed512177997cd0fd1c7804443b69063d69e8
Giuseppe <g.dipuglia@datawizard.it>
2023-04-19 18:14:58 +0200

    added packages

--- python_scripts/import_data.py
+++ python_scripts/import_data.py
@@ -29,7 +29,7 @@ os.mkdir("./output")
 resource_list = [
     'codesystem',
     'valueset',
-    'structuredefinition', #TODO: 400
+    'structuredefinition',  # TODO: 400
     'bundle',
     'medicinalproductdefinition',
     'organization',
@@ -41,6 +41,7 @@ resource_list = [
     'implementationguide',
     'searchparameter'
 ]
+
 
 def create_or_update(request_url, resource_type, resource_id, data):
     headers = {
@@ -61,8 +62,9 @@ def main():
     # file.extractall('./')
     results = {}
 
+    package_folder = "./packages"
     for item in resource_list:
-        for file_path in pathlib.Path("./package").glob('**/*.json'):
+        for file_path in pathlib.Path(package_folder).glob('**/*.json'):
             with open(file_path, "r") as json_file:
                 file_name = str(file_path).rsplit('/', 1)[1]
                 json_dict = json.load(json_file)
@@ -85,12 +87,19 @@ def main():
                 with open(request_result_filepath, "w") as outfile:
                     json.dump(a.json(), outfile, indent=2)
 
-                results.setdefault(a.status_code, []).append(file_path)
+                item_results = results.setdefault(item, {})
+                item_results.setdefault(a.status_code, []).append(file_path)
 
                 print(f"{file_path} -- {a.status_code}")
         print(f"FINISH RESOURCE {item}")
         time.sleep(3)
 
+    print("Report")
+    for item, status_codes in results.items():
+        print(f"\n- {item} -----------")
+        for status_code, resource_results in status_codes.items():
+            print(f"\tstatus {status_code}: {len(resource_results)} resources")
+    print("------------------------------")
     print("DONE!")
 
 

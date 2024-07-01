27893625de2efdddc98732039d304adb650a04d1
Simone Diaco <s.diaco@datawizard.it>
2024-07-01 15:20:19 +0200

    fix productlist loading

--- apps/productlist/assets/js/productlist.js
+++ apps/productlist/assets/js/productlist.js
@@ -1,44 +1,101 @@
-var baseurl=window.location.origin
-var url=baseurl+'/fhir/ManufacturedItemDefinition?_format=json';
-url=baseurl+'/fhir/MedicinalProductDefinition?_format=json&_count=20000';
-fetch(url)
-    .then((response) => {
-        return response.json()
-    })
-    .then((posts) => {
-        for (prod in posts.entry) {
-            var t = $('#prod-table').DataTable();
-            t.pageLength=2;
-            t.iDisplayLength= 50;
-            current_row = []
+$(document).ready(function() {
+    var baseurl = window.location.origin;
+    var url = baseurl + '/fhir/MedicinalProductDefinition?_format=json';
 
-            try { current_row.push(posts.entry[prod].resource.id)
-            } catch (error){ current_row.push(error) }
+    function buildAjaxUrl(data) {
+        var pageSize = data.length;
+        var page = data.start / pageSize;
+        var searchParam = data.search.value ? `&name:contains=${data.search.value}` : ''; // Aggiunge il parametro di ricerca se presente
+        return `${url}&_count=${pageSize}&_getpagesoffset=${pageSize * page}${searchParam}`;
+    }
 
-            try { current_row.push('<b>'+posts.entry[prod].resource.name[0].productName+'</b>')
-            } catch (error){ current_row.push(error) }
+    function fetchTotalRecords() {
+        return fetch(url + '&_count=20000')
+            .then((response) => response.json())
+            .then((data) => {
+                return data.total || 6000;
+            })
+            .catch((error) => {
+                console.error('Error fetching total records:', error);
+                return 6000;
+            });
+    }
 
-            try { current_row.push(posts.entry[prod].resource.name[0].usage[0].country.coding[0].display)
-            } catch (error){ current_row.push(error) }
-
-            try { current_row.push( '<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">Viewer</a> <br> <a target="_blank" href="'+baseurl+'/apps/visualiser/index.html?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">New Viewer</a>' )
-            } catch (error){ current_row.push(error) }
-
-            try { current_row.push( '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=xml">XML</a> <br> <a href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=json">JSON</a>' )
-            } catch (error){ current_row.push(error) }
-
-            try { current_row.push( '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'/$validate">FHIR Validation</a>' )
-            } catch (error){ current_row.push(error) }
-
-            t.row.add( current_row ).draw();
-
-//                t.row.add([
-//                    posts.entry[prod].resource.id,
-//                    '<b>'+posts.entry[prod].resource.name[0].productName,
-//                    posts.entry[prod].resource.name[0].usage[0].country.coding[0].display,
-//                    '<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">Viewer</a> <a target="_blank" href="https://jpa.unicom.datawizard.it/apps/visualiser/index.html?url='+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'">New Viewer</a>',
-//                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=xml">XML</a> <a href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'?_format=json">JSON</a>',
-//                    '<a target="_blank" href="'+baseurl+'/fhir/MedicinalProductDefinition/'+posts.entry[prod].resource.id+'/$validate">FHIR Validation</a>',
-//                ]).draw();
+    function initializeDataTable(recordsTotal) {
+        if ($.fn.DataTable.isDataTable('#prod-table')) {
+            $('#prod-table').DataTable().clear().destroy(); // Distrugge la DataTable esistente
         }
-    })
+
+        $('#prod-table').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "pageLength": 100,
+            "ajax": function(data, callback, settings) {
+                var ajaxUrl = buildAjaxUrl(data);
+
+                fetch(ajaxUrl)
+                    .then((response) => response.json())
+                    .then((posts) => {
+                        var recordsFiltered = posts.total || recordsTotal;
+
+                        var data = posts.entry.map((entry) => {
+                            var current_row = [];
+
+                            current_row.push(entry.resource.id || 'N/A');
+                            current_row.push(entry.resource.name?.[0]?.productName ? `<b>${entry.resource.name[0].productName}</b>` : 'N/A');
+                            current_row.push(entry.resource.name?.[0]?.usage?.[0]?.country?.coding?.[0]?.display || 'N/A');
+
+                            var viewerLink = `<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url=${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}">Viewer</a>
+                                              <br>
+                                              <a target="_blank" href="${baseurl}/apps/visualiser/index.html?url=${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}">New Viewer</a>`;
+                            current_row.push(viewerLink);
+
+                            var formatLinks = `<a target="_blank" href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}?_format=xml">XML</a>
+                                               <br>
+                                               <a href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}?_format=json">JSON</a>`;
+                            current_row.push(formatLinks);
+
+                            var validationLink = `<a target="_blank" href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}/$validate">FHIR Validation</a>`;
+                            current_row.push(validationLink);
+
+                            return current_row;
+                        });
+
+                        callback({
+                            draw: data.draw,
+                            recordsTotal: recordsTotal,
+                            recordsFiltered: recordsFiltered,
+                            data: data
+                        });
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching data:', error);
+                        callback({
+                            draw: data.draw,
+                            recordsTotal: 0,
+                            recordsFiltered: 0,
+                            data: []
+                        });
+                    });
+            },
+            "columns": [
+                { "title": "ID" },
+                { "title": "Name" },
+                { "title": "Country" },
+                { "title": "Viewer Links" },
+                { "title": "Formats" },
+                { "title": "Validation" }
+            ]
+        });
+    }
+
+    $.fn.dataTable.ext.errMode = 'none';
+    $('#prod-table').on('error.dt', function(e, settings, techNote, message) {
+        console.log('DataTables error:', message);
+    });
+
+    // Recupera il totale dei record e inizializza la DataTable
+    fetchTotalRecords().then(totalRecords => {
+        initializeDataTable(totalRecords); // Inizializza la DataTable con il totale dei record
+    });
+});
--- python_scripts/import_data.py
+++ python_scripts/import_data.py
@@ -27,19 +27,19 @@ if os.path.isdir(result_dir):
 os.mkdir("./output")
 
 resource_list = [
-    # 'codesystem',
-    # 'valueset',
-    # 'structuredefinition',  # TODO: 400
-    # 'bundle',
+    'codesystem',
+    'valueset',
+    'structuredefinition',  # TODO: 400
+    'bundle',
     'medicinalproductdefinition',
-    # 'organization',
-    # 'manufactureditemdefinition',
-    # 'packagedproductdefinition',
-    # 'regulatedauthorization',
-    # 'administrableproductdefinition',
-    # 'ingredient',
-    # 'implementationguide',
-    # 'searchparameter'
+    'organization',
+    'manufactureditemdefinition',
+    'packagedproductdefinition',
+    'regulatedauthorization',
+    'administrableproductdefinition',
+    'ingredient',
+    'implementationguide',
+    'searchparameter'
 ]
 
 
@@ -62,7 +62,8 @@ def main():
     # file.extractall('./')
     results = {}
 
-    package_folder = "./package_mpd_us"
+    # package_folder = "../20240322_package-ita-bel-gr"
+    package_folder = "../package_mpd_us"
     for item in resource_list:
         for file_path in pathlib.Path(package_folder).glob('**/*.json'):
             with open(file_path, "r") as json_file:

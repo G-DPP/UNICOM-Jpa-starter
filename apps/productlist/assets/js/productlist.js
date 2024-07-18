ae1557c0336208e8f870667fcdc729e69123f8ee
Simone Diaco <s.diaco@datawizard.it>
2024-07-18 16:29:27 +0200

    edited productlist

--- apps/productlist/assets/js/productlist.js
+++ apps/productlist/assets/js/productlist.js
@@ -2,28 +2,39 @@ $(document).ready(function() {
     var baseurl = window.location.origin;
     var url = baseurl + '/fhir/MedicinalProductDefinition?_format=json';
 
-    function buildAjaxUrl(data) {
+    function buildAjaxUrl(data, searchParam) {
         var pageSize = data.length;
         var page = data.start / pageSize;
-        var searchParam = data.search.value ? `&name:contains=${data.search.value}` : ''; // Aggiunge il parametro di ricerca se presente
         return `${url}&_count=${pageSize}&_getpagesoffset=${pageSize * page}${searchParam}`;
     }
 
-    function fetchTotalRecords() {
-        return fetch(url + '&_count=20000')
-            .then((response) => response.json())
-            .then((data) => {
-                return data.total || 6000;
-            })
-            .catch((error) => {
-                console.error('Error fetching total records:', error);
-                return 6000;
-            });
+    function fetchSearchResults(searchValue) {
+        var searchParams = [
+            `_id:contains=${searchValue}`,
+            `name:contains=${searchValue}`,
+            `name.usage.country.coding.display:contains=${searchValue}` //TODO
+        ];
+
+        var promises = searchParams.map(param => fetch(url + '&' + param + '&_count=10000').then(response => response.json()));
+
+        return Promise.all(promises).then(results => {
+            var combinedEntries = results.flatMap(result => result.entry || []);
+            var uniqueEntries = combinedEntries.reduce((acc, entry) => {
+                if (!acc.some(e => e.resource.id === entry.resource.id)) {
+                    acc.push(entry);
+                }
+                return acc;
+            }, []);
+            return {
+                total: uniqueEntries.length,
+                entries: uniqueEntries
+            };
+        });
     }
 
-    function initializeDataTable(recordsTotal) {
+    function initializeDataTable(totalRecords) {
         if ($.fn.DataTable.isDataTable('#prod-table')) {
-            $('#prod-table').DataTable().clear().destroy(); // Distrugge la DataTable esistente
+            $('#prod-table').DataTable().clear().destroy(); // Destroy existing DataTable
         }
 
         $('#prod-table').DataTable({
@@ -31,14 +42,13 @@ $(document).ready(function() {
             "serverSide": true,
             "pageLength": 100,
             "ajax": function(data, callback, settings) {
-                var ajaxUrl = buildAjaxUrl(data);
+                var searchValue = data.search.value;
 
-                fetch(ajaxUrl)
-                    .then((response) => response.json())
-                    .then((posts) => {
-                        var recordsFiltered = posts.total || recordsTotal;
+                $('#loading').show(); // Show loading indicator
 
-                        var data = posts.entry.map((entry) => {
+                if (searchValue) {
+                    fetchSearchResults(searchValue).then(results => {
+                        var data = results.entries.map((entry) => {
                             var current_row = [];
 
                             current_row.push(entry.resource.id || 'N/A');
@@ -63,12 +73,13 @@ $(document).ready(function() {
 
                         callback({
                             draw: data.draw,
-                            recordsTotal: recordsTotal,
-                            recordsFiltered: recordsFiltered,
+                            recordsTotal: results.total,
+                            recordsFiltered: results.total,
                             data: data
                         });
-                    })
-                    .catch((error) => {
+
+                        $('#loading').hide(); // Hide loading indicator
+                    }).catch((error) => {
                         console.error('Error fetching data:', error);
                         callback({
                             draw: data.draw,
@@ -76,7 +87,60 @@ $(document).ready(function() {
                             recordsFiltered: 0,
                             data: []
                         });
+                        $('#loading').hide(); // Hide loading indicator
                     });
+                } else {
+                    var ajaxUrl = buildAjaxUrl(data, '');
+
+                    fetch(ajaxUrl)
+                        .then((response) => response.json())
+                        .then((posts) => {
+                            var recordsTotal = posts.total || totalRecords;
+                            var recordsFiltered = recordsTotal;
+
+                            var data = posts.entry.map((entry) => {
+                                var current_row = [];
+
+                                current_row.push(entry.resource.id || 'N/A');
+                                current_row.push(entry.resource.name?.[0]?.productName ? `<b>${entry.resource.name[0].productName}</b>` : 'N/A');
+                                current_row.push(entry.resource.name?.[0]?.usage?.[0]?.country?.coding?.[0]?.display || 'N/A');
+
+                                var viewerLink = `<a target="_blank" href="https://idmp-viewer.azurewebsites.net/display-product?url=${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}">Viewer</a>
+                                                  <br>
+                                                  <a target="_blank" href="${baseurl}/apps/visualiser/index.html?url=${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}">New Viewer</a>`;
+                                current_row.push(viewerLink);
+
+                                var formatLinks = `<a target="_blank" href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}?_format=xml">XML</a>
+                                                   <br>
+                                                   <a href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}?_format=json">JSON</a>`;
+                                current_row.push(formatLinks);
+
+                                var validationLink = `<a target="_blank" href="${baseurl}/fhir/MedicinalProductDefinition/${entry.resource.id}/$validate">FHIR Validation</a>`;
+                                current_row.push(validationLink);
+
+                                return current_row;
+                            });
+
+                            callback({
+                                draw: data.draw,
+                                recordsTotal: recordsTotal,
+                                recordsFiltered: recordsFiltered,
+                                data: data
+                            });
+
+                            $('#loading').hide(); // Hide loading indicator
+                        })
+                        .catch((error) => {
+                            console.error('Error fetching data:', error);
+                            callback({
+                                draw: data.draw,
+                                recordsTotal: 0,
+                                recordsFiltered: 0,
+                                data: []
+                            });
+                            $('#loading').hide(); // Hide loading indicator
+                        });
+                }
             },
             "columns": [
                 { "title": "ID" },
@@ -89,13 +153,21 @@ $(document).ready(function() {
         });
     }
 
+    // Replace alert with console log
     $.fn.dataTable.ext.errMode = 'none';
     $('#prod-table').on('error.dt', function(e, settings, techNote, message) {
         console.log('DataTables error:', message);
     });
 
-    // Recupera il totale dei record e inizializza la DataTable
-    fetchTotalRecords().then(totalRecords => {
-        initializeDataTable(totalRecords); // Inizializza la DataTable con il totale dei record
+    $('#loading').show(); // Show loading indicator at the beginning
+
+    fetch(url + '&_count=20000').then((response) => response.json()).then((data) => {
+        var totalRecords = data.total || 6000;
+        initializeDataTable(totalRecords); // Initialize the DataTable when the page loads
+        $('#loading').hide(); // Hide loading indicator
+    }).catch((error) => {
+        console.error('Error fetching total records:', error);
+        initializeDataTable(6000); // Default value in case of error
+        $('#loading').hide(); // Hide loading indicator
     });
 });
